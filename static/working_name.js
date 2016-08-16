6ad8cff573f759d45d1e058b8ce15c7886aff52a
Rachel <rachelwshen@gmail.com>
2016-08-16 04:35:23 +0000

    working on adding a marker and commented out some doc code only have warnings in the console

--- static/working_name.js
+++ static/working_name.js
@@ -60,54 +60,54 @@ $(document).ready(function () {
     // var user_lng = $("#lng").val();
 // Random code writings that might be relvant sometime
 // from https://developers.google.com/maps/documentation/javascript/examples/directions-complex
-        function calculateAndDisplayRoute (directionsDisplay, directionsService, markerArray, stepDisplay, map) {
-        // First, remove any existing markers from the map.
-        for (var i = 0; i < markerArray.length; i++) {
-          markerArray[i].setMap(null);
-        }
+    //     function calculateAndDisplayRoute (directionsDisplay, directionsService, markerArray, stepDisplay, map) {
+    //     // First, remove any existing markers from the map.
+    //     for (var i = 0; i < markerArray.length; i++) {
+    //       markerArray[i].setMap(null);
+    //     }
 
-        // Retrieve the start and end locations and create a DirectionsRequest using
-        // WALKING directions.
-        directionsService.route({
-          origin: document.getElementById('start').value,
-          destination: document.getElementById('end').value,
-          travelMode: 'WALKING'
-        }, function(response, status) {
-          // Route the directions and pass the response to a function to create
-          // markers for each step.
-                if (status === 'OK') {
-                    document.getElementById('warnings-panel').innerHTML =
-                '<b>' + response.routes[0].warnings + '</b>';
-                    directionsDisplay.setDirections(response);
-                    showSteps(response, markerArray, stepDisplay, map);
-                } else {
-                  window.alert('Directions request failed due to ' + status);
-                }
-        });
-      }
+    //     // Retrieve the start and end locations and create a DirectionsRequest using
+    //     // WALKING directions.
+    //     directionsService.route({
+    //       origin: document.getElementById('start').value,
+    //       destination: document.getElementById('end').value,
+    //       travelMode: 'WALKING'
+    //     }, function(response, status) {
+    //       // Route the directions and pass the response to a function to create
+    //       // markers for each step.
+    //             if (status === 'OK') {
+    //                 document.getElementById('warnings-panel').innerHTML =
+    //             '<b>' + response.routes[0].warnings + '</b>';
+    //                 directionsDisplay.setDirections(response);
+    //                 showSteps(response, markerArray, stepDisplay, map);
+    //             } else {
+    //               window.alert('Directions request failed due to ' + status);
+    //             }
+    //     });
+    //   }
 
-    function showSteps(directionResult, markerArray, stepDisplay, map) {
-        // For each step, place a marker, and add the text to the marker's infowindow.
-        // Also attach the marker to an array so we can keep track of it and remove it
-        // when calculating new routes.
-        var myRoute = directionResult.routes[0].legs[0];
-        for (var i = 0; i < myRoute.steps.length; i++) {
-          var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
-          marker.setMap(map);
-          marker.setPosition(myRoute.steps[i].start_location);
-          attachInstructionText(
-              stepDisplay, marker, myRoute.steps[i].instructions, map);
-        }
-      }
+    // function showSteps(directionResult, markerArray, stepDisplay, map) {
+    //     // For each step, place a marker, and add the text to the marker's infowindow.
+    //     // Also attach the marker to an array so we can keep track of it and remove it
+    //     // when calculating new routes.
+    //     var myRoute = directionResult.routes[0].legs[0];
+    //     for (var i = 0; i < myRoute.steps.length; i++) {
+    //       var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+    //       marker.setMap(map);
+    //       marker.setPosition(myRoute.steps[i].start_location);
+    //       attachInstructionText(
+    //           stepDisplay, marker, myRoute.steps[i].instructions, map);
+    //     }
+    //   }
 
-    function attachInstructionText(stepDisplay, marker, text, map) {
-        google.maps.event.addListener(marker, 'click', function() {
-          // Open an info window when the marker is clicked on, containing the text
-          // of the step.
-          stepDisplay.setContent(text);
-          stepDisplay.open(map, marker);
-        });
-      }
+    // function attachInstructionText(stepDisplay, marker, text, map) {
+    //     google.maps.event.addListener(marker, 'click', function() {
+    //       // Open an info window when the marker is clicked on, containing the text
+    //       // of the step.
+    //       stepDisplay.setContent(text);
+    //       stepDisplay.open(map, marker);
+    //     });
+    //   }
 
 //      window.onload = function() {
 //         var startPos;
@@ -125,22 +125,12 @@ $(document).ready(function () {
 
 // var map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-//FROM LECTURE NOTES ADDING MARKERS 
-    function addMarker() {
-      var image = "image url"
-      var nearUserLocation = new google.maps.LatLng(user_location)
-    }
-        
-
 
 
 //user_lat and user_lng are just parameters 
 function initMap(user_lat, user_lng) {
-  debugger;
 
     var hackbright = {lat: 37.7886679, lng: -122.4114987};
-
-
 
     var map = new google.maps.Map(document.getElementById('homepage-map'), {
           center: {lat:parseInt(user_lat), lng: parseInt(user_lng) },
@@ -159,15 +149,15 @@ function initMap(user_lat, user_lng) {
         var stepDisplay = new google.maps.InfoWindow;
 
         // Display the route between the initial start and end selections.
-        calculateAndDisplayRoute(
-            directionsDisplay, directionsService, markerArray, stepDisplay, map);
+        // calculateAndDisplayRoute(
+        //     directionsDisplay, directionsService, markerArray, stepDisplay, map);
         // Listen to change events from the start and end lists.
-        var onChangeHandler = function() {
-          calculateAndDisplayRoute(
-              directionsDisplay, directionsService, markerArray, stepDisplay, map);
-        };
-        document.getElementById('start').addEventListener('change', onChangeHandler);
-        document.getElementById('end').addEventListener('change', onChangeHandler);
+        // var onChangeHandler = function() {
+        //   calculateAndDisplayRoute(
+        //       directionsDisplay, directionsService, markerArray, stepDisplay, map);
+        // };
+        // document.getElementById('start').addEventListener('change', onChangeHandler);
+        // document.getElementById('end').addEventListener('change', onChangeHandler);
       }
 
 google.maps.event.addDomListener(window, "load", getLocation);
@@ -180,17 +170,37 @@ function getLocation() {
         console.log("Geolocation is not supported by the browser.");
     } 
   } 
+
 function handlePositionFound(position) { //get the coords
     console.log("Latitude: "+ position.coords.latitude + "<br> Longitude: " + position.coords.longitude);
-  $("#lat").val(position.coords.latitude); //user_lat
-  $("#lng").val(position.coords.longitude); //user_lng
+  var userLat = $("#lat").val(position.coords.latitude); //user_lat
+  var userLng = $("#lng").val(position.coords.longitude); //user_lng
 
-  initMap(position.coords.latitude, position.coords.longitude);
-  }
+  var marker = new google.maps.Marker({
+    position: {lat:parseInt(userLat), lng: parseInt(userLng)},
+    setMap: 'homepage-map',
+    title: 'User location!'
+  }); 
+
+initMap(position.coords.latitude, position.coords.longitude);
+}
+
+
   
-
-
-
+//FROM LECTURE NOTES ADDING MARKERS 
+// function addMarker() {
+//   var myImageURL = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+//   var image = myImageURL;
+//   var nearHackbright = new google.maps.LatLng(37.7886679, -122.4114987)
+//   var marker = new google.maps.Marker({
+//                 position = nearHackbright,
+//                 map: map, 
+//                 title: 'Hover text',
+//                 icon: image
+//   });
+//   return marker;
+// }
+// var marker = addMarker()
 
 
 
