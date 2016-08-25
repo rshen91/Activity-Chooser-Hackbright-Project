03a7e3ebc01fc6a27ab4a9589ff7d5ee11c1c744
Rachel <rachelwshen@gmail.com>
2016-08-25 00:22:25 +0000

    added radio buttons to the homepage and moved some directions code that isnt relevant yet produces alert message currently

--- static/homepage.js
+++ static/homepage.js
@@ -1,11 +1,4 @@
 "use strict";
-// in the callback from google maps directions service
-var lenOfDirections = response.routes[0].legs[0].steps.length;
-var halfWayDirectionIndex = lenOfDirections/2;
-var theHalfwayDirection = response.routes[0[].legs[0].step[halfWayDirectionIndex];
-var theHalfwayDirectionLatLng = {lat: theHalfwayDirection.end_point.lat(), lng: theHalfwayDirection.end_point.lng()};
-response.routes[0].legs[0].steps[4].end_point.lng()
-
 
 $(document).ready(function () {
 console.log("HEY YOU, I AM HERE in the document.ready");
@@ -25,8 +18,12 @@ function initMap(user_lat, user_lng) {
     });    
 
         var userMarker = addMarker(map, user_lat, user_lng);
+        var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);    
+        displayDirections(map);
+    }
+
         
-      } //this is where initMap ends
 // this is needed to load the map - triggers the cascade of functions 
 google.maps.event.addDomListener(window, "load", getLocation);
 
@@ -62,7 +59,47 @@ function addMarker(map, user_lat, user_lng) {
 
 }
 
+function displayDirections(map) {
+        var user_lat = $("#user_lat").val();
+        var user_lng = $("#user_lng").val();
+        var activity_lat = $("#activity_lat").val();
+        var activity_lng = $("#activity_lng").val();
+        var end_lat = $("#end_lat").val();
+        var end_lng = $("#end_lng").val();
+        var user_lat = $("#user_lat").val();
+        var user_lng = $("#user_lng").val();
+        var activityWaypoint = {
+            location: {lat: Number(activity_lat), lng: Number(activity_lng)},
+            stopover: true
+        };
+        var routeOptions = {
+            origin: {lat: Number(user_lat), lng: Number(user_lng)},
+            destination: {lat: Number(end_lat), lng: Number(end_lng)},
+            waypoints: [activityWaypoint],
+            travelMode: google.maps.TravelMode.DRIVING
+        };
 
+        var directionsService = new google.maps.DirectionsService;
+        directionsService.route(routeOptions, function(response, status) {
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+
+            } else {
+                window.alert('Directions request failed due to ' + status);
+            }
+        });
+
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+
+        directionsDisplay.setMap(map);
+    }
+
+// in the callback from google maps directions service
+// var lenOfDirections = response.routes[0].legs[0].steps.length;
+// var halfWayDirectionIndex = lenOfDirections/2;
+// var theHalfwayDirection = response.routes[0[].legs[0].step[halfWayDirectionIndex];
+// var theHalfwayDirectionLatLng = {lat: theHalfwayDirection.end_point.lat(), lng: theHalfwayDirection.end_point.lng()};
+// response.routes[0].legs[0].steps[4].end_point.lng()
 });   //end documentReady
 
 
